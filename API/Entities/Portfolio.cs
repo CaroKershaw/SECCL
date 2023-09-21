538dd9cc492925022a5b479dbffa9c8c98de0f6f
Caroline Kershaw <callcaro@gmail.com>
2023-09-21 12:07:46 +0100

    Create portfolio class

---
+++ API/Entities/Portfolio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Entities;
+
+public class Portfolio
+{
+    public string Id { get; set; }
+    public string FirmId { get; set; }
+    public string Name { get; set; }
+    public List<string> NodeIds { get; set; }
+    public List<string> NodeNames { get; set; }
+    public string Status { get; set; }
+    public string Currency { get; set; }
+    public decimal UninvestedCash { get; set; }
+    public decimal Growth { get; set; }
+    public decimal GrowthPercent { get; set; }
+}
