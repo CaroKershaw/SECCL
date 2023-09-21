22b6a34a4651573e13681b0580fdafc7f98cba73
Caroline Kershaw <callcaro@gmail.com>
2023-09-21 13:03:39 +0100

    Add a dbContext class

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions options) : base(options) 
+    {
+
+    }
+
+    public DbSet<Portfolio> Portfolios { get; set; }
+
+}
