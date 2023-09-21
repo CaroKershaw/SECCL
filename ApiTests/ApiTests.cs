1f4976cde5877a0b55dcda2130e797bf20067e0f
Caroline Kershaw <callcaro@gmail.com>
2023-09-21 12:37:21 +0100

    Create APITests project and placeholder ApiTests

---
+++ ApiTests/ApiTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using API.Controllers;
+
+
+namespace ApiTests
+{
+    public class ApiTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange (set up test data and dependencies)
+
+            // Act (perform the action you're testing)
+
+            // Assert (verify the expected outcome)
+        }
+
+        // Add more test methods as needed
+    }
+}
